780ca762987d5f0659e514bd87c185061ec72cff
Piterden <efremov.a.denis@gmail.com>
2019-03-07 10:54:51 +0300

    Added repo and array of types check.

--- index.js
+++ index.js
@@ -7,35 +7,61 @@ class BaseClass {
       throw new Error(`The ${this.constructor.name} constructor must receive at least ${keys.length} argument${keys.length > 1 && 's'}!!!`)
     }
 
+    const validateType = (arg, key, idx, instance) => {
+      const type = instance && instance.name ? instance.name.toLowerCase() : 'object'
+
+      switch (typeof arg) {
+        case 'array':
+          if (!Array.isArray(arg)) {
+            throw new TypeError(`The argument ${idx + 1} ('${key}') must be 'Array' type!!!`)
+          }
+          break
+
+        case 'object':
+          if (!(arg instanceof instance || arg.constructor.name === instance.name)) {
+            throw new TypeError(`The argument ${idx + 1} ('${key}') must be '${instance.name}' type!!!`)
+          }
+          break
+
+        case 'string':
+        case 'number':
+        case 'boolean':
+          if (typeof arg !== type) { // eslint-disable-line valid-typeof
+            throw new TypeError(`The argument ${idx + 1} ('${key}') must be '${instance && instance.name}' type!!!`)
+          }
+          break
+
+        default:
+      }
+    }
+
     Object.assign(this, args.reduce((acc, arg, idx) => {
       const key = keys[idx]
 
       if (typeof key !== 'undefined') {
         const instance = this.args[key]
-        const type = instance && instance.name ? instance.name.toLowerCase() : 'object'
 
-        switch (typeof arg) {
-          case 'array':
-            if (!Array.isArray(arg)) {
-              throw new TypeError(`The argument ${idx + 1} ('${key}') must be 'Array' type!!!`)
+        if (typeof instance === 'function') {
+          validateType(arg, key, idx, instance)
+        }
+
+        if (Array.isArray(instance)) {
+          let pass = false
+          let err = null
+
+          instance.forEach((inst) => {
+            try {
+              validateType(arg, key, idx, inst)
+              pass = true
             }
-            break
-
-          case 'object':
-            if (!(arg instanceof instance || arg.constructor.name === instance.name)) {
-              throw new TypeError(`The argument ${idx + 1} ('${key}') must be '${instance.name}' type!!!`)
+            catch (error) {
+              err = error
             }
-            break
+          })
 
-          case 'string':
-          case 'number':
-          case 'boolean':
-            if (typeof arg !== type) { // eslint-disable-line valid-typeof
-              throw new TypeError(`The argument ${idx + 1} ('${key}') must be '${instance && instance.name}' type!!!`)
-            }
-            break
-
-          default:
+          if (!pass) {
+            throw err
+          }
         }
 
         acc[key] = arg
